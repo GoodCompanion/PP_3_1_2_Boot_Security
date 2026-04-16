248a4a520488aa893b4680f5bfe044c512b0ffb7
Vladimir Kalinin <mr.vladimar.kalinin2000@mail.ru>
2026-04-16 15:30:05 +0400

    Переименовал класс

--- src/main/java/ru/kata/spring/boot_security/demo/model/Role.java
+++ src/main/java/ru/kata/spring/boot_security/demo/model/Role.java
@@ -2,7 +2,7 @@ package ru.kata.spring.boot_security.demo.model;
 
 import org.springframework.security.core.GrantedAuthority;
 
-public class Rore implements GrantedAuthority {
+public class Role implements GrantedAuthority {
 
     @Override
     public String getAuthority() {
