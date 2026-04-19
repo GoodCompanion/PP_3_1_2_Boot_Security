f2dd52fd9af3fa0d7f3f579d647f15264f167b02
Vladimir Kalinin <mr.vladimar.kalinin2000@mail.ru>
2026-04-19 15:24:17 +0400

    Реализовал метод loadUserByUsername

--- src/main/java/ru/kata/spring/boot_security/demo/service/UserDetailsServiceImpl.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserDetailsServiceImpl.java
@@ -18,6 +18,6 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        return userDao.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("Пользователь с таким username не найден"));
     }
 }
