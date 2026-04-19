ec6f454a29f4d5635b57148ebd91552a17e788c8
Vladimir Kalinin <mr.vladimar.kalinin2000@mail.ru>
2026-04-19 16:00:28 +0400

    Довил зависимость от роли в сервистный класс UserServiceImpl и изменил тестовый метод добавив роли пользователям

--- src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
@@ -3,21 +3,26 @@ package ru.kata.spring.boot_security.demo.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import ru.kata.spring.boot_security.demo.model.Role;
 import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.repository.RoleDao;
 import ru.kata.spring.boot_security.demo.repository.UserDao;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
+import java.util.Set;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     private final UserDao userDao;
+    private final RoleDao roleDao;
     private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserServiceImpl(UserDao userDao, PasswordEncoder passwordEncoder) {
+    public UserServiceImpl(UserDao userDao, RoleDao roleDao, PasswordEncoder passwordEncoder) {
         this.userDao = userDao;
+        this.roleDao = roleDao;
         this.passwordEncoder = passwordEncoder;
     }
 
@@ -50,12 +55,20 @@ public class UserServiceImpl implements UserService {
         userDao.deleteById(id);
     }
 
-    // временные пользователи для теста
+    // временные пользователи и роли для теста
     @PostConstruct
     public void init() {
         if (userDao.count() == 0) {
-            User admin = new User("admin", "admin", "Владимир", "Калинин", 25);
-            User user = new User("user", "user", "Эрик", "Цой", 24);
+            Role adminRole = new Role("ROLE_ADMIN");
+            Role userRole = new Role("ROLE_USER");
+
+            if (roleDao.count() == 0) {
+                roleDao.save(adminRole);
+                roleDao.save(userRole);
+            }
+
+            User admin = new User("admin", "admin", "Владимир", "Калинин", 25, Set.of(adminRole, userRole));
+            User user = new User("user", "user", "Эрик", "Цой", 24, Set.of(userRole));
             addUser(admin);
             addUser(user);
         }
