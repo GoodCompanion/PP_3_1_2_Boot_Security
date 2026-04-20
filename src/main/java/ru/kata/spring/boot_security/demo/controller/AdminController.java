a6793948895b4ce3f149fdc85bdcb09310d397bb
Vladimir Kalinin <mr.vladimar.kalinin2000@mail.ru>
2026-04-20 16:05:45 +0400

    Реализовал отдельный контроллер для роли admin

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -1,9 +1,95 @@
 package ru.kata.spring.boot_security.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.model.Role;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.service.RoleService;
+import ru.kata.spring.boot_security.demo.service.UserService;
+
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
+    private final UserService userService;
+    private final RoleService roleService;
+
+    @Autowired
+    public AdminController(UserService userService, RoleService roleService) {
+        this.userService = userService;
+        this.roleService = roleService;
+    }
+
+    @GetMapping
+    public String adminPage(Model model) {
+        model.addAttribute("users", userService.getUsers());
+        return "admin";
+    }
+
+    @GetMapping("/add")
+    public String showAddForm(Model model) {
+        model.addAttribute("user", new User());
+        model.addAttribute("allRoles", roleService.getAllRoles());
+        return "add";
+    }
+
+    @PostMapping("/add")
+    public String addUser(@RequestParam("username") String username,
+                          @RequestParam("password") String password,
+                          @RequestParam("name") String name,
+                          @RequestParam("surname") String surname,
+                          @RequestParam("age") int age,
+                          @RequestParam("roleIds") Set<Long> roleIds) {
+        User user = new User(username, password, name, surname, age);
+        if (roleIds != null && !roleIds.isEmpty()) {
+            Set<Role> roles = roleIds.stream().map(roleService::getRoleById).collect(Collectors.toSet());
+            user.setRoles(roles);
+        }
+        userService.addUser(user);
+        return "redirect:/admin";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String showEditForm(@PathVariable("id") Long id, Model model) {
+        User user = userService.getUser(id);
+        model.addAttribute("user", user);
+        model.addAttribute("allRoles", roleService.getAllRoles());
+        return "edit";
+    }
+
+    @PostMapping("/edit")
+    public String updateUser(@RequestParam("id") Long id,
+                             @RequestParam("username") String username,
+                             @RequestParam("password") String password,
+                             @RequestParam("name") String name,
+                             @RequestParam("surname") String surname,
+                             @RequestParam("age") int age,
+                             @RequestParam("roleIds") Set<Long> roleIds) {
+        User user = userService.getUser(id);
+        user.setUsername(username);
+        if (password != null && !password.isEmpty()) {
+            user.setPassword(password);
+        }
+        user.setName(name);
+        user.setSurname(surname);
+        user.setAge(age);
+
+        if (roleIds != null && !roleIds.isEmpty()) {
+            Set<Role> roles = roleIds.stream().map(roleService::getRoleById).collect(Collectors.toSet());
+            user.setRoles(roles);
+        }
+        userService.updateUser(user);
+        return "redirect:/admin";
+    }
+
+    @PostMapping("/delete/{id}")
+    public String deleteUser(@PathVariable("id") Long id) {
+        userService.deleteUser(id);
+        return "redirect:/admin";
+    }
 }
+
