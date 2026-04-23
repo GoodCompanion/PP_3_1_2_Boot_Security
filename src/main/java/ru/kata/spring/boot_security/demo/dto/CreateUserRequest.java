7f20e2577985685baa1a3a281b7a7b86b67d74d8
Vladimir Kalinin <mr.vladimar.kalinin2000@mail.ru>
2026-04-23 17:53:41 +0400

    Создал DTO класс

---
+++ src/main/java/ru/kata/spring/boot_security/demo/dto/CreateUserRequest.java
@@ -0,0 +1,81 @@
+package ru.kata.spring.boot_security.demo.dto;
+
+import ru.kata.spring.boot_security.demo.model.Role;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+public class CreateUserRequest {
+    @NotBlank(message = "Логин обязателен")
+    @Size(min = 1, max = 50)
+    private String username;
+
+    @NotBlank(message = "Пароль обязателен")
+    @Size(min = 1, max = 50)
+    private String password;
+
+    @NotBlank(message = "Имя обязательно")
+    @Size(min = 1)
+    private String name;
+
+    @NotBlank(message = "Фамилия обязательна")
+    @Size(min = 1)
+    private String surname;
+
+    @Min(value = 0)
+    @Max(value = 150)
+    private int age;
+
+    private List<Role> roleIds;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public List<Role> getRoleIds() {
+        return roleIds;
+    }
+
+    public void setRoleIds(List<Role> roleIds) {
+        this.roleIds = roleIds;
+    }
+}
